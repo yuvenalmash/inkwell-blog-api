778b816e43464d1ac887d6a80ce27ff568d7be3e
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-07 18:26:44 +0300

    Add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
+  has_many :follows, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :messages, dependent: :destroy
+  has_many :followers, through: :follows
 end
