de806009a32c6621fcd975cd7ce14f0b487186b8
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-07 17:48:48 +0300

    Create User model

---
+++ db/migrate/20230507142908_create_users.rb
@@ -0,0 +1,17 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false, limit: 20
+      t.string :email, null: false, limit: 50
+      t.string :password_digest, null: false
+      t.string :avatar, null: false, default: ""
+      t.string :bio, null: false, default: ""
+      t.integer :posts_count, null: false, default: 0
+      t.integer :comments_count, null: false, default: 0
+      t.integer :likes_count, null: false, default: 0
+      t.boolean :is_admin, null: false, default: false
+
+      t.timestamps
+    end
+  end
+end
