7c1f0a9153be09ad7fd65d2e8f62b3ce78f6e47a
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-09 01:55:47 +0300

    Add likes controller and update its model validations

---
+++ app/controllers/api/v1/likes_controller.rb
@@ -0,0 +1,56 @@
+class Api::V1::LikesController < ApplicationController
+  before_action :set_user, only: %i[index create destroy]
+  before_action :set_post, only: %i[index create destroy]
+  before_action :set_comment, only: %i[index create destroy]
+  before_action :set_likeable, only: %i[index create destroy]
+  before_action :set_like, only: %i[destroy]
+
+  def index
+    @likes = @likeable.likes
+    render json: @likes, include: { user: { only: %i[username avatar] } }
+  end
+
+  def create
+    like = @likeable.likes.new(user: @user)
+    if like.save
+      render json: like, status: :created
+    else
+      render json: { errors: like.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user == @like.user
+      @like.destroy
+      render json: @like, status: :no_content
+    else
+      render json: { errors: 'You are not authorized to perform this action' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def set_comment
+    @comment = @post.comments.find(params[:comment_id]) if params[:comment_id]
+  end
+
+  def set_like
+    @like = @likeable.likes.find(params[:id])
+  end
+
+  def set_likeable
+    if @comment
+      @likeable = @comment
+    elsif @post
+      @likeable = @post
+    end
+  end
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,7 +2,7 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :likeable, polymorphic: true
 
-  validates :user_id, presence: true
-  validates :likeable_id, presence: true
-  validates :likeable_type, presence: true
+  # validates :user_id, presence: true
+  # validates :likeable_id, presence: true
+  # validates :likeable_type, presence: true
 end
