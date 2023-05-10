f7c6838569e12cfbcbd7f1c279c8b602194929b6
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-10 23:40:51 +0300

    Add follow counts in users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_10_113943) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_10_160620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -82,6 +82,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_10_113943) do
     t.boolean "is_admin", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "followers_count", default: 0, null: false
+    t.integer "followed_count", default: 0, null: false
   end
 
   add_foreign_key "comments", "posts"
