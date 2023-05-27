0d97237bb21ced74b4123f1c394703092f05fa9e
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-05-27 23:03:06 +0300

    Configure devise and jwt-devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable,,:recoverable, :rememberable, :validatable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :jwt_authenticatable, jwt_revocation_strategy: Devise::JWT::RevocationStrategies::Null
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :follows, dependent: :destroy, foreign_key: :follower_id
@@ -9,4 +13,5 @@ class User < ApplicationRecord
   has_many :liked_posts, through: :likes, source: :likeable, source_type: 'Post'
 
   validates :username, presence: true, uniqueness: true, length: { maximum: 20 }
+  validates :email, presence: true, uniqueness: true
 end
