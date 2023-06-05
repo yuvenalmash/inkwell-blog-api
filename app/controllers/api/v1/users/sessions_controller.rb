4dc250525fc6a09246436be80149cc4878afeb93
Yuvenal Njoroge <yuvenal.njoroge@gmail.com>
2023-06-05 19:48:04 +0300

    return user on login

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
     user = User.find_for_database_authentication(email: params[:user][:email])
 
     if user&.valid_password?(params[:user][:password])
-      render json: { token: user.generate_jwt }, status: :ok
+      render json: { token: user.generate_jwt, user: user }, status: :ok
     else
       render json: { error: 'Invalid email or password' }, status: :unauthorized
     end
